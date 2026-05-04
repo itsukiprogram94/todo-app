a2940230e7dfef7e9ac5fcfcb8ed4af4144e2b2d
水谷樹 <itsuki_0904@mizutanijunoMacBook-Pro.local>
2026-05-04 13:18:47 +0900

    feat: タグのCRUD機能の実装とREADMEの追加

--- backend/src/tag/tag.service.ts
+++ backend/src/tag/tag.service.ts
@@ -16,4 +16,18 @@ export class TagService {
       data: { name: name, color: color },
     });
   }
+  updateTag(id: number, name: string, color?: string) {
+    return this.prisma.tag.update({
+      where: { id: id }, // どのIDのタグを更新するか指定
+      data: { name: name, color: color }, // 新しい名前と色
+    });
+  }
+
+  // ★追加：タグを削除する処理
+  deleteTag(id: number) {
+    return this.prisma.tag.delete({
+      where: { id: id }, // どのIDのタグを削除するか指定
+    });
+  }
 }
+
